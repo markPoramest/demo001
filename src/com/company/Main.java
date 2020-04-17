bd5774f09316b0ccb365532c1dd0d927d80aa258
markza1998 <2411kjn@gmail.com>
2020-04-17 13:27:28 +0700

    00

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("Hello World\tMy name is Poramest");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input number 1 : ");
+        int n1 = scan.nextInt();
+        System.out.print("Input number 2 : ");
+        int n2 = scan.nextInt();
+        System.out.println("number1 + number 2 : "+(n1+n2));
+        System.out.println("number1 - number 2 : "+(n1-n2));
+        System.out.println("number1 * number 2 : "+(n1*n2));
+        System.out.printf("number1 / number 2 : %.2f",(float)n1/n2);
+
     }
 }
